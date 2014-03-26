0ec4c19d970daec5d546d733f013ecbeda58d83c
unknown <plemanach@hotmail.com>
2014-03-26 00:01:46 +0000

    init commit

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "exzmq", description: "ØMQ for Elixir",
+                 version: "0.1", keywords: ["Elixir","Erlang","zmq","zeromq","ØMQ"],
+                 maintainers: [[name: "Patrice Le Manach", email: "plemanach@hotmail.com"]],
+                 repositories: [[github: "plemanach/exzmq", tag: "0.1"]])

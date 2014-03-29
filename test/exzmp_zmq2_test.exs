ee25b5a7e9d86c2a787ee777e7642a4d258b3f78
unknown <plemanach@hotmail.com>
2014-03-29 13:08:30 +0000

    Add erlzmq test suite

--- test/exzmp_zmq2_test.exs
+++ test/exzmp_zmq2_test.exs
@@ -16,6 +16,20 @@ defmodule Exzmq.ZMQ3Test do
     ok = Exzmq.close(s2)
     ok = :erlzmq.term(c)
   end
+
+  def basic_tests_ezmq(fun, transport, ip, port, type1, type2, mode, size) do
+    basic_tests_ezmq(fun, transport, ip, Port, type1, [], type2, [], mode, size)
+  end
+
+  def basic_tests_ezmq(fun, transport, ip, port, type1, id1, type2, id2, mode, size) do
+    {:ok, c} = :erlzmq.context(1)
+    {s1, s2} = create_bound_pair_ezmq(c, type1, id1, type2, id2, mode, transport, ip, port)
+    msg = String.duplicate("X", size)
+    fun({s1, s2}, msg, mode)
+    :ok = Exzmq.close(s1)
+    :ok = :erlzmq.close(s2)
+    :ok = :erlzmq.term(c)
+  end
   
   def create_bound_pair_erlzmq(ctx, type1, id1, type2, id2, mode, transport, ip, port) do
     
@@ -40,10 +54,95 @@ defmodule Exzmq.ZMQ3Test do
   def erlzmq_identity(socket, id) do
     :erlzmq.setsockopt(socket, :identity, id)
   end
+ 
+  test "reqrep_tcp_test_active" do
+    basic_tests_erlzmq(&ping_pong_erlzmq/3, 'tcp://127.0.0.1:5555', {127,0,0,1}, 5555, :req, :rep, :active, 3)
+    basic_tests_ezmq(&ping_pong_ezmq/3, 'tcp://127.0.0.1:5555', {127,0,0,1}, 5555, :req, :rep, :active, 3)
+  end
+
+  test "reqrep_tcp_test_passive" do
+    basic_tests_erlzmq(&ping_pong_erlzmq/3, 'tcp://127.0.0.1:5556', {127,0,0,1}, 5556, :req, :rep, :passive, 3)
+    basic_tests_ezmq(&ping_pong_ezmq/3, 'tcp://127.0.0.1:5556', {127,0,0,1}, 5556, :req, :rep, :passive, 3)
+  end
 
   test "reqrep_tcp_id_test_active" do
     basic_tests_erlzmq(&ping_pong_erlzmq/3, 'tcp://127.0.0.1:5555', {127,0,0,1}, 5555, :req, "reqrep_tcp_id_test_active_req", :rep, "reqrep_tcp_id_test_active_rep", :active, 3)
     basic_tests_ezmq(&ping_pong_ezmq/3, 'tcp://127.0.0.1:5555', {127,0,0,1}, 5555, :req, "reqrep_tcp_id_test_active_req", :rep, "reqrep_tcp_id_test_active_rep", :active, 3)
+  end
+
+  test "reqrep_tcp_id_test_passive" do
+    basic_tests_erlzmq(&ping_pong_erlzmq/3, 'tcp://127.0.0.1:5556', {127,0,0,1}, 5556, :req, "reqrep_tcp_id_test_passive_req", :rep, "reqrep_tcp_id_test_passive_rep", :passive, 3)
+    basic_tests_ezmq(&ping_pong_ezmq/3, 'tcp://127.0.0.1:5556', {127,0,0,1}, 5556, :req, "reqrep_tcp_id_test_passive_req", :rep, "reqrep_tcp_id_test_passive_rep", :passive, 3)
+  end
+
+  test "reqrep_tcp_large_active" do
+    basic_tests_erlzmq(&ping_pong_erlzmq/3, 'tcp://127.0.0.1:5557', {127,0,0,1}, 5557, :req, :rep, :active, 256)
+    basic_tests_ezmq(&ping_pong_ezmq/3, 'tcp://127.0.0.1:5557', {127,0,0,1}, 5557, :req, :rep, :active, 256)
+  end
+
+  test "reqrep_tcp_large_passive" do
+    basic_tests_erlzmq(&ping_pong_erlzmq/3, 'tcp://127.0.0.1:5558', {127,0,0,1}, 5558, :req, :rep, :passive, 256)
+    basic_tests_ezmq(&ping_pong_ezmq/3, 'tcp://127.0.0.1:5558', {127,0,0,1}, 5558, :req, :rep, :passive, 256)
+  end
+ 
+  test "dealerrep_tcp_test_active" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_dealer/3, 'tcp://127.0.0.1:5559', {127,0,0,1}, 5559, :dealer, :rep, :active, 4)
+    basic_tests_ezmq(&ping_pong_ezmq_dealer/3, 'tcp://127.0.0.1:5559', {127,0,0,1}, 5559, :dealer, :rep, :active, 4)
+  end
+
+  test "dealerrep_tcp_test_passive" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_dealer/3, 'tcp://127.0.0.1:5560', {127,0,0,1}, 5560, :dealer, :rep, :passive, 3)
+    basic_tests_ezmq(&ping_pong_ezmq_dealer/3, 'tcp://127.0.0.1:5560', {127,0,0,1}, 5560, :dealer, :rep, :passive, 3)
+  end
+  
+  test "reqdealer_tcp_id_test_active" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_dealer/3, 'tcp://127.0.0.1:5561', {127,0,0,1}, 5561, :req, "reqdealer_tcp_id_test_active_req", :dealer, "reqdealer_tcp_test_active_dealer", :active, 3)
+    basic_tests_ezmq(&ping_pong_ezmq_dealer/3, 'tcp://127.0.0.1:5561', {127,0,0,1}, 5561, :req, "reqdealer_tcp_id_test_active_req", :dealer, "reqdealer_tcp_test_active_dealer", :active, 3)
+  end
+
+  test "reqdealer_tcp_id_test_passive" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_dealer/3, 'tcp://127.0.0.1:5562', {127,0,0,1}, 5562, :req, "reqdealer_tcp_id_test_passive_req", :dealer, "reqdealer_tcp_test_passive_dealer", :active, 3)
+    basic_tests_ezmq(&ping_pong_ezmq_dealer/3, 'tcp://127.0.0.1:5562', {127,0,0,1}, 5562, :req, "reqdealer_tcp_id_test_passive_req", :dealer, "reqdealer_tcp_test_passive_dealer", :active, 3)
+  end
+
+  test "reqrouter_tcp_test_active" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_router/3, 'tcp://127.0.0.1:5561', {127,0,0,1}, 5561, :req, :router, :active, 3)
+    basic_tests_ezmq(&ping_pong_ezmq_router/3, 'tcp://127.0.0.1:5561', {127,0,0,1}, 5561, :req, :router, :active, 3)
+  end
+
+  test "reqrouter_tcp_test_passive" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_router/3, 'tcp://127.0.0.1:5562', {127,0,0,1}, 5562, :req, :router, :passive, 3)
+    basic_tests_ezmq(&ping_pong_ezmq_router/3, 'tcp://127.0.0.1:5562', {127,0,0,1}, 5562, :req, :router, :passive, 3)
+  end
+
+  test "reqrouter_tcp_id_test_active" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_router/3, 'tcp://127.0.0.1:5561', {127,0,0,1}, 5561, :req, "reqrouter_tcp_test_active_req", :router, "reqrouter_tcp_test_active_router", :active, 3)
+    basic_tests_ezmq(&ping_pong_ezmq_router/3, 'tcp://127.0.0.1:5561', {127,0,0,1}, 5561, :req, "reqrouter_tcp_test_active_req", :router, "reqrouter_tcp_test_active_router", :active, 3)
+  end
+
+  test "reqrouter_tcp_id_test_passive" do
+    basic_tests_erlzmq(&ping_pong_erlzmq_router/3, 'tcp://127.0.0.1:5562', {127,0,0,1}, 5562, :req, "reqrouter_tcp_test_active_req", :router, "reqrouter_tcp_test_active_router", :passive, 3)
+    basic_tests_ezmq(&ping_pong_ezmq_router/3, 'tcp://127.0.0.1:5562', {127,0,0,1}, 5562, :req, "reqrouter_tcp_test_active_req", :router, "reqrouter_tcp_test_active_router", :passive, 3)
+  end
+   
+  def ping_pong_erlzmq({s1, s2}, msg, :active) do
+    :ok = :erlzmq.send(s1, msg, [:sndmore])
+    :ok = :erlzmq.send(s1, msg)
+    assert_mbox({:zmq, s2, [msg,msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s2, [msg])
+    assert_mbox({:zmq, s1, msg, []})
+
+    :ok = :erlzmq.send(s1, msg)
+    assert_mbox({:zmq, s2, [msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s2, [msg])
+    assert_mbox({:zmq, s1, msg, []})
+    assert_mbox_empty()
+
+    :ok
   end
 
   def ping_pong_erlzmq({s1, s2}, msg, :active) do
@@ -64,15 +163,170 @@ defmodule Exzmq.ZMQ3Test do
     assert_mbox_empty()
 
     :ok
-   end
+  end
 
-   ## assert that message queue is empty....
+  def ping_pong_erlzmq({s1, s2}, msg, :passive) do
+    :ok = :erlzmq.send(s1, msg)
+    {:ok, [msg]} = Exzmq.recv(s2)
+    :ok = Exzmq.send(s2, [msg])
+    {:ok, msg} = :erlzmq.recv(s1)
+    :ok = :erlzmq.send(s1, msg, [:sndmore])
+    :ok = :erlzmq.send(s1, msg)
+    {:ok, [msg,msg]} = Exzmq.recv(s2)
+    :ok
+  end
+
+  def ping_pong_ezmq({s1, s2}, msg, :active) do
+    :ok = Exzmq.send(s1, [msg,msg])
+    assert_mbox({:zmq, s2, msg, [:rcvmore]})
+    assert_mbox({:zmq, s2, msg, []})
+    assert_mbox_empty()
+
+    :ok = :erlzmq.send(s2, msg)
+    assert_mbox({:zmq, s1, [msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s1, [msg])
+    assert_mbox({:zmq, s2, msg, []})
+    assert_mbox_empty()
+
+    :ok = :erlzmq.send(s2, msg, [:sndmore])
+    :ok = :erlzmq.send(s2, msg)
+    assert_mbox({zmq, s1, [msg,msg]})
+    assert_mbox_empty()
+
+    :ok
+  end
+      
+  def ping_pong_ezmq({s1, s2}, msg, passive) do
+    :ok = Exzmq.send(s1, [msg])
+    {:ok, msg} = :erlzmq.recv(s2)
+    :ok = :erlzmq.send(s2, msg)
+    {:ok, [msg]} = Exzmq.recv(s1)
+    :ok = Exzmq.send(s1, [msg,msg])
+    {:ok, msg} = :erlzmq.recv(s2)
+    {:ok, msg} = :erlzmq.recv(s2)
+    :ok
+  end
+
+  def ping_pong_erlzmq_dealer({s1, s2}, msg, active) do
+    :ok = :erlzmq.send(s1, msg, [:sndmore])
+    :ok = :erlzmq.send(s1, msg)
+    assert_mbox({:zmq, s2, [msg,msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s2, [msg])
+    assert_mbox({:zmq, s1, msg, []})
+
+    :ok = :erlzmq.send(s1, msg)
+    assert_mbox({:zmq, s2, [msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s2, [msg])
+    assert_mbox({zmq, s1, msg, []})
+    assert_mbox_empty()
+
+    :ok
+  end
+      
+  def ping_pong_erlzmq_dealer({s1, s2}, msg, :passive) do
+    :ok = :erlzmq.send(s1, msg)
+    {:ok, [msg]} = Exzmq.recv(s2)
+    :ok = Exzmq.send(s2, [msg])
+    {:ok, msg} = :erlzmq.recv(s1)
+    :ok = :erlzmq.send(s1, msg, [:sndmore])
+    :ok = :erlzmq.send(s1, msg)
+    {:ok, [msg,msg]} = Exzmq.recv(s2)
+    :ok
+  end
+
+  def ping_pong_ezmq_dealer({s1, s2}, msg, :active) do
+    :ok = Exzmq.send(s1, [msg,msg])
+    assert_mbox({:zmq, s2, <<>>, [:rcvmore]})
+    assert_mbox({:zmq, s2, msg, [:rcvmore]})
+    assert_mbox({:zmq, s2, msg, []})
+    assert_mbox_empty()
+
+    :ok = :erlzmq.send(s2, <<>>, [:sndmore])
+    :ok = :erlzmq.send(s2, msg)
+    assert_mbox({:zmq, s1, [msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s1, [msg])
+    assert_mbox({:zmq, s2, <<>>, [:rcvmore]})
+    assert_mbox({:zmq, s2, msg, []})
+    assert_mbox_empty()
+
+    :ok = :erlzmq.send(s2, <<>>, [:sndmore])
+    :ok = :erlzmq.send(s2, msg, [:sndmore])
+    :ok = :erlzmq.send(s2, msg)
+    assert_mbox({:zmq, s1, [msg,msg]})
+    assert_mbox_empty()
+
+    :ok
+  end
+      
+  def ping_pong_ezmq_dealer({s1, s2}, msg, :passive) do
+    :ok = Exzmq.send(s1, [msg])
+    {:ok, <<>>} = :erlzmq.recv(s2)
+    {:ok, msg} = :erlzmq.recv(s2)
+    :ok = :erlzmq.send(s2, <<>>, [sndmore])
+    :ok = :erlzmq.send(s2, msg)
+    {:ok, [msg]} = Exzmq.recv(s1)
+    :ok = Exzmq.send(s1, [msg,msg])
+    {:ok, <<>>} = :erlzmq.recv(s2)
+    {:ok, msg} = :erlzmq.recv(s2)
+    {:ok, msg} = :erlzmq.recv(s2)
+    :ok
+  end
+
+  def dealer_ping_pong_erlzmq({s1, s2}, msg, active) do
+    :ok = :erlzmq.send(s1, <<>>, [:sndmore])
+    :ok = :erlzmq.send(s1, msg, [:sndmore])
+    :ok = :erlzmq.send(s1, msg)
+
+    assert_mbox({zmq, s2, [msg,msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s2, [msg])
+    assert_mbox({zmq, s1, <<>>, [:rcvmore]})
+    assert_mbox({zmq, s1, msg, []})
+    assert_mbox_empty()
+
+    :ok = :erlzmq.send(s1, <<>>, [:sndmore])
+    :ok = :erlzmq.send(s1, msg)
+    assert_mbox({zmq, s2, [msg]})
+    assert_mbox_empty()
+
+    :ok = Exzmq.send(s2, [msg])
+    assert_mbox({zmq, s1, <<>>, [:rcvmore]})
+    assert_mbox({zmq, s1, msg, []})
+    assert_mbox_empty()
+
+    :ok
+  end
+      
+  def dealer_ping_pong_erlzmq({s1, s2}, msg, :passive) do
+    :ok = :erlzmq.send(s1, <<>>, [:sndmore])
+    :ok = :erlzmq.send(s1, msg)
+    {:ok, [msg]} = Exzmq.recv(s2)
+    :ok = Exzmq.send(s2, [msg])
+    {:ok, <<>>} = :erlzmq.recv(s1)
+    {:ok, msg} = :erlzmq.recv(s1)
+    :ok = :erlzmq.send(s1, <<>>, [sndmore])
+    :ok = :erlzmq.send(s1, msg, [sndmore])
+    :ok = :erlzmq.send(s1, msg)
+    {:ok, [msg,msg]} = Exzmq.recv(s2)
+    :ok
+  end
+
+  ## assert that message queue is empty....
   def assert_mbox_empty() do
     receive do
-       m -> assert false
-	 after
-	   0 -> :ok
-	end
+      m -> assert false
+	  after
+	    0 -> :ok
+  	end
   end
 
   ## assert that top message in the queue is what we think it should be
